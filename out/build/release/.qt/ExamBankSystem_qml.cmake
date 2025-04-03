6b3b4a43a8f457bf4201524e3e252d3d5ddd7a8e
HuanLing4763 <2043417914@qq.com>
2025-04-03 16:03:34 +0800

    Remove out folder

--- out/build/release/.qt/ExamBankSystem_qml.cmake
+++
@@ -1,9 +0,0 @@
-
-set(target "ExamBankSystem")
-set(working_dir "E:/Project/CPP/ExamBankSystem")
-set(src_and_dest_list
-    "E:/Project/CPP/ExamBankSystem/qml/Loading.qml"
-    "E:/Project/CPP/ExamBankSystem/out/build/release/ExamBankSystem/qml/Loading.qml"
-
-)
-set(timestamp_file "E:/Project/CPP/ExamBankSystem/out/build/release/.qt/ExamBankSystem_qml.txt")
